9bbc34323c41cf91d6417e018f70622f0714113c
gabrielsimonetti62-hub <gabriel.simonetti62@gmail.com>
2026-07-17 10:57:17 +0200

    Update page.js

--- app/articles/aides-accompagnement-tpe-pme/page.js
+++ app/articles/aides-accompagnement-tpe-pme/page.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import { Nav, Footer } from "../../components";
+import { Nav, Footer, ConversionBanner } from "../../components";
 
 export const metadata = {
   title: "Aides et accompagnement pour les TPE et PME | ATLAS",
-  description: "Panorama complet des aides publiques, dispositifs d'accompagnement et ressources disponibles pour les dirigeants de TPE et PME en France.",
+  description: "Panorama des aides publiques, dispositifs d'accompagnement et ressources pour les dirigeants de TPE et PME en France.",
 };
 
 export default function Article4() {
@@ -16,50 +16,31 @@ export default function Article4() {
           <h1>Aides et accompagnement pour les TPE et PME</h1>
         </section>
       </div>
-      <div className="container"><hr className="divider" /></div>
       <div className="container">
-        <section className="section article-body">
-
-          <p>Le paysage des aides aux entreprises en France is riche mais complexe. Entre les dispositifs nationaux, r&eacute;gionaux, sectoriels et priv&eacute;s, un dirigeant de TPE ou PME peut facilement passer &agrave; c&ocirc;t&eacute; d&apos;opportunit&eacute;s qui lui sont destin&eacute;es. Cet article dresse un panorama des principales ressources disponibles, class&eacute;es par type de besoin.</p>
+        <div className="article-body-white">
+          <p>Le paysage des aides aux entreprises en France est riche mais complexe. Un dirigeant peut facilement passer &agrave; c&ocirc;t&eacute; d&apos;opportunit&eacute;s qui lui sont destin&eacute;es.</p>
 
           <h2>Financement et tr&eacute;sorerie</h2>
-          <p><a href="https://www.bpifrance.fr" target="_blank" rel="noopener">Bpifrance</a> est l&apos;interlocuteur de r&eacute;f&eacute;rence pour le financement des entreprises fran&ccedil;aises. L&apos;organisme propose des pr&ecirc;ts &agrave; taux pr&eacute;f&eacute;rentiel, des garanties de pr&ecirc;t bancaire (qui facilitent l&apos;obtention d&apos;un cr&eacute;dit aupr&egrave;s de votre banque), et des aides &agrave; l&apos;innovation. Le <a href="https://www.bpifrance.fr/catalogue-offres/financement/pret-tpe" target="_blank" rel="noopener">Pr&ecirc;t TPE</a> est sp&eacute;cifiquement con&ccedil;u pour les entreprises de moins de 50 salari&eacute;s.</p>
-          <p><a href="https://www.france-active.org" target="_blank" rel="noopener">France Active</a> accompagne les entrepreneurs dans l&apos;acc&egrave;s au financement, notamment par des garanties d&apos;emprunt et des pr&ecirc;ts solidaires. Le r&eacute;seau est particuli&egrave;rement adapt&eacute; aux cr&eacute;ateurs et aux TPE en phase de d&eacute;veloppement.</p>
-          <p>L&apos;<a href="https://www.adie.org" target="_blank" rel="noopener">Adie</a> (Association pour le Droit &agrave; l&apos;Initiative &Eacute;conomique) propose des micropr&ecirc;ts jusqu&apos;&agrave; 12 000 &euro; pour les entrepreneurs qui n&apos;ont pas acc&egrave;s au cr&eacute;dit bancaire classique.</p>
+          <p><a href="https://www.bpifrance.fr" target="_blank" rel="noopener">Bpifrance</a> propose pr&ecirc;ts &agrave; taux pr&eacute;f&eacute;rentiel, garanties de pr&ecirc;t bancaire et aides &agrave; l&apos;innovation. Le Pr&ecirc;t TPE vise sp&eacute;cifiquement les moins de 50 salari&eacute;s. <a href="https://www.france-active.org" target="_blank" rel="noopener">France Active</a> accompagne via des garanties d&apos;emprunt et pr&ecirc;ts solidaires. L&apos;<a href="https://www.adie.org" target="_blank" rel="noopener">Adie</a> propose des micropr&ecirc;ts jusqu&apos;&agrave; 12 000 &euro;.</p>
 
-          <h2>Conseil et accompagnement strat&eacute;gique</h2>
-          <p>Les <a href="https://www.cci.fr" target="_blank" rel="noopener">Chambres de Commerce et d&apos;Industrie (CCI)</a> proposent des rendez-vous individuels gratuits avec des conseillers sp&eacute;cialis&eacute;s, des ateliers th&eacute;matiques et des programmes d&apos;accompagnement. Chaque CCI d&eacute;partementale dispose d&apos;une offre propre &mdash; renseignez-vous aupr&egrave;s de la v&ocirc;tre.</p>
-          <p>Les <a href="https://www.cma-france.fr" target="_blank" rel="noopener">Chambres de M&eacute;tiers et de l&apos;Artisanat (CMA)</a> remplissent le m&ecirc;me r&ocirc;le pour les artisans et les entreprises artisanales. Elles proposent notamment des formations &agrave; la gestion, au num&eacute;rique et au d&eacute;veloppement commercial.</p>
-          <p>Les r&eacute;seaux d&apos;accompagnement comme <a href="https://www.reseau-entreprendre.org" target="_blank" rel="noopener">R&eacute;seau Entreprendre</a>, <a href="https://www.initiative-france.fr" target="_blank" rel="noopener">Initiative France</a> et <a href="https://www.bfranceactive.org" target="_blank" rel="noopener">BGE</a> proposent du mentorat, des pr&ecirc;ts d&apos;honneur et un accompagnement par des chefs d&apos;entreprise b&eacute;n&eacute;voles. Ces dispositifs sont particuli&egrave;rement pr&eacute;cieux pour les dirigeants qui souffrent d&apos;isolement d&eacute;cisionnel.</p>
+          <h2>Conseil et accompagnement</h2>
+          <p>Les <a href="https://www.cci.fr" target="_blank" rel="noopener">CCI</a> et <a href="https://www.cma-france.fr" target="_blank" rel="noopener">CMA</a> proposent rendez-vous individuels, ateliers et programmes d&apos;accompagnement. <a href="https://www.reseau-entreprendre.org" target="_blank" rel="noopener">R&eacute;seau Entreprendre</a>, <a href="https://www.initiative-france.fr" target="_blank" rel="noopener">Initiative France</a> et <a href="https://www.bfranceactive.org" target="_blank" rel="noopener">BGE</a> offrent mentorat et pr&ecirc;ts d&apos;honneur.</p>
 
-          <h2>Formation et mont&eacute;e en comp&eacute;tences</h2>
-          <p>Les <a href="https://travail-emploi.gouv.fr/ministere/missions-organisation/article/les-opco" target="_blank" rel="noopener">OPCO</a> (Op&eacute;rateurs de Comp&eacute;tences) financent la formation professionnelle des salari&eacute;s et des dirigeants. Chaque secteur d&apos;activit&eacute; est rattach&eacute; &agrave; un OPCO sp&eacute;cifique. Par exemple, <a href="https://www.constructys.fr" target="_blank" rel="noopener">Constructys</a> pour le b&acirc;timent, <a href="https://www.opcommerce.com" target="_blank" rel="noopener">OPCO Commerce</a> pour le commerce, ou <a href="https://www.akto.fr" target="_blank" rel="noopener">AKTO</a> pour les services. Les budgets de formation sont souvent sous-utilis&eacute;s par les TPE &mdash; renseignez-vous sur vos droits.</p>
-          <p><a href="https://www.francetravail.fr" target="_blank" rel="noopener">France Travail</a> (anciennement P&ocirc;le Emploi) propose des dispositifs d&apos;aide au recrutement et &agrave; la formation des demandeurs d&apos;emploi que vous souhaitez int&eacute;grer, comme la Pr&eacute;paration Op&eacute;rationnelle &agrave; l&apos;Emploi (POE) ou l&apos;aide &agrave; l&apos;apprentissage.</p>
+          <h2>Formation</h2>
+          <p>Les <a href="https://travail-emploi.gouv.fr/ministere/missions-organisation/article/les-opco" target="_blank" rel="noopener">OPCO</a> financent la formation. Chaque secteur a le sien : <a href="https://www.constructys.fr" target="_blank" rel="noopener">Constructys</a> pour le b&acirc;timent, <a href="https://www.opcommerce.com" target="_blank" rel="noopener">OPCO Commerce</a>, <a href="https://www.akto.fr" target="_blank" rel="noopener">AKTO</a> pour les services. <a href="https://www.francetravail.fr" target="_blank" rel="noopener">France Travail</a> propose des aides au recrutement et &agrave; la formation.</p>
 
           <h2>Transformation num&eacute;rique</h2>
-          <p><a href="https://www.francenum.gouv.fr" target="_blank" rel="noopener">France Num</a> est le portail gouvernemental d&eacute;di&eacute; &agrave; la transformation num&eacute;rique des TPE et PME. Il propose des diagnostics num&eacute;riques gratuits, un annuaire d&apos;accompagnateurs r&eacute;f&eacute;renc&eacute;s, et des informations sur les aides financi&egrave;res disponibles pour investir dans le digital (site web, logiciels, CRM, outils de gestion).</p>
-          <p>Certaines r&eacute;gions compl&egrave;tent ces dispositifs par des ch&egrave;ques num&eacute;riques &mdash; des aides directes de 500 &agrave; 5 000 &euro; pour financer l&apos;acquisition d&apos;outils ou de prestations de service num&eacute;riques. Consultez le site de votre conseil r&eacute;gional pour conna&icirc;tre les dispositifs en vigueur.</p>
+          <p><a href="https://www.francenum.gouv.fr" target="_blank" rel="noopener">France Num</a> propose diagnostics num&eacute;riques gratuits et annuaire d&apos;accompagnateurs. Certaines r&eacute;gions compl&egrave;tent par des ch&egrave;ques num&eacute;riques de 500 &agrave; 5 000 &euro;.</p>
 
           <h2>Aides sectorielles</h2>
-          <p>Au-del&agrave; des dispositifs transversaux, chaque secteur d&apos;activit&eacute; dispose de ses propres organismes et aides.</p>
-          <p>Pour les artisans du b&acirc;timent : la <a href="https://www.capeb.fr" target="_blank" rel="noopener">CAPEB</a> (Conf&eacute;d&eacute;ration de l&apos;Artisanat et des Petites Entreprises du B&acirc;timent) et la <a href="https://www.ffbatiment.fr" target="_blank" rel="noopener">FFB</a> (F&eacute;d&eacute;ration Fran&ccedil;aise du B&acirc;timent) proposent des services juridiques, des formations et des veilles r&eacute;glementaires.</p>
-          <p>Pour la r&eacute;novation &eacute;nerg&eacute;tique : <a href="https://france-renov.gouv.fr" target="_blank" rel="noopener">France R&eacute;nov&apos;</a> et l&apos;<a href="https://www.anah.gouv.fr" target="_blank" rel="noopener">ANAH</a> (Agence Nationale de l&apos;Habitat) g&egrave;rent les dispositifs MaPrimeR&eacute;nov&apos; et les certificats d&apos;&eacute;conomie d&apos;&eacute;nergie.</p>
-          <p>Pour le commerce : les f&eacute;d&eacute;rations de commer&ccedil;ants locales, le <a href="https://www.fisac.fr" target="_blank" rel="noopener">FISAC</a> (Fonds d&apos;Intervention pour les Services, l&apos;Artisanat et le Commerce) dans les territoires qui en b&eacute;n&eacute;ficient encore.</p>
-          <p>Pour les entreprises innovantes : le <a href="https://www.enseignementsup-recherche.gouv.fr/fr/le-credit-d-impot-recherche-cir-49465" target="_blank" rel="noopener">Cr&eacute;dit d&apos;Imp&ocirc;t Recherche (CIR)</a> et le statut de Jeune Entreprise Innovante (JEI) offrent des avantages fiscaux significatifs.</p>
+          <p>B&acirc;timent : <a href="https://www.capeb.fr" target="_blank" rel="noopener">CAPEB</a> et <a href="https://www.ffbatiment.fr" target="_blank" rel="noopener">FFB</a>. R&eacute;novation &eacute;nerg&eacute;tique : <a href="https://france-renov.gouv.fr" target="_blank" rel="noopener">France R&eacute;nov&apos;</a> et <a href="https://www.anah.gouv.fr" target="_blank" rel="noopener">ANAH</a>. Cr&eacute;ation : <a href="https://www.urssaf.fr/accueil/outils-documentation/acre.html" target="_blank" rel="noopener">ACRE</a> pour l&apos;exon&eacute;ration de cotisations. Le site <a href="https://les-aides.fr" target="_blank" rel="noopener">les-aides.fr</a> permet de rechercher les aides par projet et r&eacute;gion.</p>
 
-          <h2>Cr&eacute;ation et reprise d&apos;entreprise</h2>
-          <p>L&apos;<a href="https://www.urssaf.fr/accueil/outils-documentation/acre.html" target="_blank" rel="noopener">ACRE</a> (Aide &agrave; la Cr&eacute;ation ou &agrave; la Reprise d&apos;une Entreprise) offre une exon&eacute;ration partielle des cotisations sociales pendant les premi&egrave;res ann&eacute;es d&apos;activit&eacute;.</p>
-          <p>Le <a href="https://www.service-public.fr/particuliers/vosdroits/F11677" target="_blank" rel="noopener">NACRE</a> (Nouvel Accompagnement &agrave; la Cr&eacute;ation ou la Reprise d&apos;Entreprise) propose un accompagnement personnalis&eacute; de trois ans avec un pr&ecirc;t &agrave; taux z&eacute;ro.</p>
-
-          <h2>Comment s&apos;y retrouver</h2>
-          <p>Le point d&apos;entr&eacute;e le plus efficace est souvent votre CCI ou CMA locale : un rendez-vous avec un conseiller permet de faire le point sur les dispositifs auxquels vous &ecirc;tes &eacute;ligible en fonction de votre secteur, votre taille et votre situation.</p>
-          <p>Le site <a href="https://les-aides.fr" target="_blank" rel="noopener">les-aides.fr</a> de la CCI permet de rechercher les aides disponibles par type de projet et par r&eacute;gion.</p>
-          <p>Pour un panorama plus large incluant l&apos;analyse de votre situation sp&eacute;cifique et des recommandations personnalis&eacute;es, le diagnostic ATLAS int&egrave;gre une identification des dispositifs pertinents pour votre entreprise dans son plan d&apos;action.</p>
-
-          <div className="article-cta">
-            <Link href="/#offres" className="btn-cta">D&eacute;couvrir le diagnostic ATLAS &rarr;</Link>
-          </div>
-        </section>
+          <ConversionBanner
+            title="Identifiez les aides qui vous concernent"
+            text="Le diagnostic ATLAS int&egrave;gre une identification des dispositifs pertinents dans son plan d&apos;action personnalis&eacute;."
+            stat="&Agrave; partir de 199 &euro; &middot; Plan d&apos;action avec ressources concr&egrave;tes"
+          />
+        </div>
       </div>
       <Footer />
     </>
